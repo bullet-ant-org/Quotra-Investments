100eb93b96a2eb44e1ab5398ffa13eac15b5eb9a
bullet-ant-org <classicesteban86@gmail.com>
2025-06-08 03:16:47 +0100

    hello final edit

--- src/layout/secSection.jsx
+++ src/layout/secSection.jsx
@@ -15,7 +15,7 @@ const SecSection = () => {
             </h1>
           </div>
           <div className="col-md-5 text-center">
-            <img src={Nonee} alt="" className="image-fluid text-center" height={500}/>
+            <img src={Nonee} alt="" className="image-fluid text-center" height={450}/>
           </div>
         </div>
       </div>
